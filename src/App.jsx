bd10643f252812958d4e554c9dc5e167ae2eaa9b
dungpro712 <deomuonnick1@gmail.com>
2026-04-18 18:02:08 +0700

    Add the complete React portfolio component code to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          This is the main page of my portfolio.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
